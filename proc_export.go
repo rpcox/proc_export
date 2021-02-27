107fb74564f475018a3dbdc985c6a4e4bdb1c741
Ric Cox <ric.cox@gmail.com>
2021-02-27 12:13:30 -0700

    proto plus

--- proc_export.go
+++ proc_export.go
@@ -2,10 +2,11 @@
 package main
 
 import (
+  "bytes"
   "bufio"
   //"encoding/json"
   "flag"
-  //"fmt"
+//  "fmt"
   "io/ioutil"
   "log"
   "net/http"
@@ -69,6 +70,34 @@ func findPids(proc_map map[string]string) {
   }
 }
 
+
+// Set the metric TYPE statement.  All of the metrics will have
+// the prefix 'proc_stat'
+func setMetricType(r []byte, m string, t string) []byte {
+  r = append(r, '#',' ','T','Y','P','E',' ','p','r','o','c','_','s','t','a','t','_')
+  line := m + " " + t + "\n"
+  return append(r, line...)
+}
+
+func setMetric(response []byte, name string, metric []byte) []byte {
+  metric_prefix := []byte{'p','r','o','c','_','s','t','a','t','_'}
+  response = append(append(append(response, metric_prefix[:]...), name...), ' ')
+  response = append(response, metric...)
+  return append(response, 10)
+}
+
+// Set a particular metric to identify if the monitored process is active
+// process running = 1, process not running = 0
+func procStatus(response []byte, proc string, status string) []byte {
+  response = append(response, '#',' ','T','Y','P','E',' ','p','r','o','c','_','s','t','a','t','_')
+  line := proc + "_up gauge\n"
+  response = append(response, line...)
+  response = append(response, 'p','r','o','c','_','s','t','a','t','_')
+  line = proc + "_up " + status + "\n"
+  return append(response, line...)
+}
+
+
 //
 func getStats(proc_map map[string]string) []byte {
   var response []byte
@@ -77,16 +106,41 @@ func getStats(proc_map map[string]string) []byte {
     content, err := ioutil.ReadFile(path)
 
     if err == nil {
-      response = append(response, content...)
+      response = procStatus(response, k, "1")
+      fields := bytes.Split(content, []byte(" "))
+      response = setMetricType(response, "minflt", "gauge")
+      response = setMetric(response, "minflt", fields[9])
+      response = setMetricType(response, "cminflt", "gauge")
+      response = setMetric(response, "cminflt", fields[10])
+      response = setMetricType(response, "majflt", "gauge")
+      response = setMetric(response, "majflt", fields[11])
+      response = setMetricType(response, "cmajflt", "gauge")
+      response = setMetric(response, "cmajflt", fields[12])
+      response = setMetricType(response, "utime_seconds", "counter")
+      response = setMetric(response, "utime_seconds", fields[13])
+      response = setMetricType(response, "stime_seconds", "counter")
+      response = setMetric(response, "stime_seconds", fields[14])
+      response = setMetricType(response, "nice", "gauge")
+      response = setMetric(response, "nice", fields[18])
+      response = setMetricType(response, "num_threads", "gauge")
+      response = setMetric(response, "num_threads", fields[19])
+      response = setMetricType(response, "vsize_bytes", "gauge")
+      response = setMetric(response, "vsize_bytes", fields[22])
+      response = setMetricType(response, "rss_pages", "gauge")
+      response = setMetric(response, "rss_pages", fields[23])
+      response = setMetricType(response, "rt_priority", "gauge")
+      response = setMetric(response, "rt_priority", fields[39])
+
     } else {
-      response = append(response, k + ": failed collection"...)
-      delete (proc_map, k)   // the process at this PID is no more
+      response = procStatus(response, k, "0")
+      delete (proc_map, k)
       continue
     }
   }
 
   return response
 }
+
 
 func main() {
   flag.Parse()
